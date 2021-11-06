f8a202d3d171413f45b0e87055735da2cba7b4ca
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-11-06 11:31:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,55 @@ console.log(name4); // "Bob"
 console.log(age4); // 26
 
 // task3
+function mul(...rest) {
+   let filtredArr = rest.filter(item => (typeof item === 'number') && ( item !== 0));
+   let result;
+   if (filtredArr.length !== 0) result = filtredArr.reduce((a, b) => a * b);
+   else result = 0;
+   return result;
+};
+
+console.log(mul(1, "str", 2, 3, true)); // 6
+console.log(mul(null, "str", false, true)); // 0
+console.log(mul(1, "str", 2, 3, true, 0)); // 0
+
+// task4
+let server = {
+   data: 0,
+   convertToString: function (callback) {
+      callback(() => this.data + "");
+   }
+};
+
+let client = {
+   server: server,
+   result: "",
+   calc: function (data) {
+      this.server.data = data;
+      this.server.convertToString(this.notification());
+   },
+   notification: function () {
+      return ((callback) => this.result = callback());
+   }
+};
+
+client.calc(123);
+
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
+
+//task5
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+
+function mapBuilder(keysArray, valuesArrays){
+   let newMap = new Map();
+   for (let i = 0; i < keysArray.length; i++) {
+      newMap.set(keysArray[i], valuesArrays[i]);
+   };
+   return newMap;
+};
+
+console.log(map.size); // 4
+console.log(map.get(2)); // "span"
